fb9f6b3bcb46fb2b436719b5b5d40c0e520b27a2
tina-hello <77915071+tina-hello@users.noreply.github.com>
2022-10-05 05:29:27 +0700

    Create index.js

---
+++ functions/index.js
@@ -0,0 +1,30 @@
+export const onRequestGet = async ({ request}) => 
+{
+
+    const clientUA = request.headers.get('User-Agent');
+    const clientIP = request.headers.get('CF-Connecting-IP');
+    const clientASN = request.cf.asn;
+    const clientISP = request.cf.asOrganization;
+    const clientCO = request.cf.country;
+    const clientCI = request.cf.city;
+    const clientRE = request.cf.region;
+    const clientLAT = request.cf.latitude;
+    const clientLON = request.cf.longitude;
+    const clientPC = request.cf.postalCode;
+    const clientTZ = request.cf.timezone;
+
+    return new Response("Public IP: " + clientIP + "\n" +
+        "ASN: " + clientASN + "\n" +
+        "ISP: " + clientISP + "\n" +
+        "Country: " + clientCO + "\n" +
+        "City: " + clientCI + "\n" +
+        "Region: " + clientRE + "\n" +
+        "Latitude, Longitude: " + clientLAT + "," + clientLON + "\n" +
+        "Postal Code: " + clientPC + "\n" +
+        "Timezone: " + clientTZ + "\n" +
+        "User Agent: " + clientUA + "\n", {
+            status: 200
+        }
+    );
+
+}
