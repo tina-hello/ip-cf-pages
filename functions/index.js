7816eb0e0823e02893b9eed0a5a9416a428f1d12
Tina <77915071+tina-hello@users.noreply.github.com>
2024-01-27 20:46:43 +0700

    Json fixed

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,8 @@
 export const onRequestGet = async ({
   request,
 }) => {
-  const clientUA = request.headers.get('User-Agent');
-  const clientIP = request.headers.get('CF-Connecting-IP');
+  const clientUA = request.headers.get(`User-Agent`);
+  const clientIP = request.headers.get(`CF-Connecting-IP`);
   const clientASN = request.cf.asn;
   const clientISP = request.cf.asOrganization;
   const cloudflareColo = request.cf.colo;
@@ -17,39 +17,38 @@ export const onRequestGet = async ({
   const clientPC = request.cf.postalCode;
   const clientTZ = request.cf.timezone;
   if (clientUA.includes("Mozilla")) {
-    return new Response('Public IP: ${clientIP}\n'
-            + 'ASN: ${clientASN}\n'
-            + 'ISP: ${clientISP}\n'
-            + 'Cloudflare Data Center: ${cloudflareColo}\n'
-            + 'HTTP Protocol: ${httpProtocol}\n'
-            + 'TLS Cipher ${tlsCipher}\n'
-            + 'TLS Version ${tlsVersion}\n'
-            + 'Country: ${clientCO}\n'
-            + 'City: ${clientCI}\n'
-            + 'Region: ${clientRE}\n'
-            + 'Latitude, Longitude: ${clientLAT},${clientLON}\n'
-            + 'Postal Code: ${clientPC}\n'
-            + 'Timezone: ${clientTZ}\n'
-            + 'User Agent: ${clientUA}\n', {
+    return new Response(`Public IP: ${clientIP}\n`
+            + `ASN: ${clientASN}\n`
+            + `ISP: ${clientISP}\n`
+            + `Cloudflare Data Center: ${cloudflareColo}\n`
+            + `HTTP Protocol: ${httpProtocol}\n`
+            + `TLS Cipher: ${tlsCipher}\n`
+            + `TLS Version: ${tlsVersion}\n`
+            + `Country: ${clientCO}\n`
+            + `City: ${clientCI}\n`
+            + `Region: ${clientRE}\n`
+            + `Latitude, Longitude: ${clientLAT},${clientLON}\n`
+            + `Postal Code: ${clientPC}\n`
+            + `Timezone: ${clientTZ}\n`
+            + `User Agent: ${clientUA}\n`, {
       status: 200,
     });
   }
   else { 
     return new Response(
-    '{\n "ip": "${clientIP}"\n'
-            + ' "asn": "${clientASN}"\n'
-            + ' "isp": "${clientISP}"\n'
-            + ' "cf-dc": "${cloudflareColo}"\n'
-            + ' "protocol": "${httpProtocol}"\n'
-            + ' "cipher: "${tlsCipher}"\n'
-            + ' "tls-version${tlsVersion}\n'
-            + ' "country": "${clientCO}"\n'
-            + ' "city": "${clientCI}"\n'
-            + ' "region": "${clientRE}"\n'
-            + ' "loc": "${clientLAT},${clientLON}"\n'
-            + ' "postal": "${clientPC}"\n'
-            + ' "timezone": "${clientTZ}"\n'
-            + ' "user-agent": "${clientUA}"\n}\n',
+    `{\n "ip": "${clientIP}",\n`
+            + ` "org": "AS${clientASN} ${clientISP}",\n`
+            + ` "cfdc": "${cloudflareColo}",\n`
+            + ` "protocol": "${httpProtocol}",\n`
+            + ` "cipher": "${tlsCipher}",\n`
+            + ` "tlsversion": "${tlsVersion}",\n`
+            + ` "country": "${clientCO}",\n`
+            + ` "city": "${clientCI}",\n`
+            + ` "region": "${clientRE}",\n`
+            + ` "loc": "${clientLAT},${clientLON}",\n`
+            + ` "postal": "${clientPC}",\n`
+            + ` "timezone": "${clientTZ}",\n`
+            + ` "useragent": "${clientUA}"\n}\n`,
 
     {
       status: 200,
